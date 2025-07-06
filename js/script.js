f8c612d3b398f363b2b7a513d4ce3de76346efd1
AshutoshThings <ashutoshv9648@gmail.com>
2025-07-06 15:19:45 +0530

    bare minimum ui addition

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
 let choices = ["rock", "paper", "scissors"];
 let computerScore = 0;
 let playerScore = 0;
+let humanScoreElem = document.querySelector(".netHumanScore");
+let computerScoreElem = document.querySelector(".netComputerScore");
+humanScoreElem.textContent = 0;
+computerScoreElem.textContent = 0;
 function getComputerChoice(){
 	let num = Math.floor(Math.random()*3);
-	//console.log(`Computer's Choice : ${choices[num]}`);
 	let computerChoice = document.querySelector(".computerChoice");
 	computerChoice.textContent = `Computer has chosen : ${choices[num]}`;
 	return choices[num];
 }
 
 function getHumanChoice(e){
-	//console.log("Human Choice : ");
-	//console.log(e.srcElement.innerText);
 	return e.srcElement.innerText;
 }
 
@@ -19,32 +20,31 @@ function playRound(computer, human){
 	let result = document.querySelector(".result");
 	if(computer == "rock" && human =="paper")
 	{
-		//console.log("You won this round!");
 		playerScore++;
 		result.textContent = "You won this round!";
+		humanScoreElem.textContent = playerScore;
 	}
 	else if(computer == "paper" && human =="scissor")
 	{
-		//console.log("You won this round!");
 		playerScore++;
 		result.textContent = "You won this round!";
+		humanScoreElem.textContent = playerScore;
 	}
 	else if(computer == "scissors" && human=="rock")
 	{
-		//console.log("You won this round!");
 		result.textContent = "You won this round!";
 		playerScore++;
+		humanScoreElem.textContent = playerScore;
 	}
 	else if(computer == human)
 	{
-		//console.log("This round is a draw");
 		result.textContent = "This round is a draw!";
 	}
 	else
 	{
-		//console.log("Computer won this round!");
 		computerScore++;
 		result.textContent = "Computer won this round!";
+		computerScoreElem.textContent = computerScore;
 	}
 	
 }
@@ -59,21 +59,13 @@ function playGame(e)
 		playRound(computerDes, humanDes);
 	//}
 }
-// 	if(computerScore>playerScore)
-// 	{
-// 		console.log("Computer Won! Game over");
-// 	}
-// 	else if(playerScore>computerScore)
-// 	{
-// 		console.log("Player Won! Game over");
-// 	}
-// 	else 
-// 	{
-// 		console.log("Game over! it's a draw");
-// 	}
-// }
-
-// playGame();
 
 let input = document.querySelectorAll("span");
 input.forEach(inp => inp.addEventListener('click',playGame ));
+
+input.forEach(inp => inp.addEventListener('click',()=>{
+	inp.style.backgroundColor = "red";
+	setTimeout(() => {
+      inp.style.backgroundColor = "mediumpurple";
+    }, 20);
+}));
